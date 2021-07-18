39b919dc5a082988b3c5e4dda5cb15f51cde90ef
Dhiviya16 <82600789+Dhiviya16@users.noreply.github.com>
2021-07-18 16:08:36 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,242 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<html>
+<head>
+	<meta charset="utf-8">
+
+<!-- To control the page's dimensions and scaling -->
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	
+	<title>Fixups</title> <!--Title-->
+
+<!-- Link to the css page -->
+	<link rel="stylesheet" href="style.css">
+
+<!-- Bootstrap stylesheets -->
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+	<script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<script src="https://kit.fontawesome.com/1d908533b5.js" crossorigin="anonymous"></script>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+	<link rel ="stylesheet" href ="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.4.0/js/bootstrap.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/2.4.0/umd/popper.min.js"></script>
+	<link rel="stylesheet "href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.4.1/css/bootstrap.min.css">
+	<link rel="stylesheet "href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.1/css/bootstrap.min.css">
+
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+
+<!-- jQuery library -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+<!-- Popper JS -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+
+<!-- Latest compiled JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+	
+</head>
+<body>
+<!-- Responsive collapsing navbar -->
+	<input type="checkbox" id="check">
+	<label for="check" class="checkbtn">
+		<i class="fas fa-bars"></i>
+	</label>
+
+	<div class ="banner-area">
+		<header>
+				<a href="index.php"><img src="images/logo.png"></a>
+
+				<ul class= "menu">
+					<li><a href="services.php">Our Services</a></li>
+					<li><a href="about.php">About Us</a></li>
+					<li><a href="join.php">Join with us</a></li>
+					<li><a href="login.php">Login/Sign Up</a></li>
+				</ul>
+		</header>
+
+			
+			<div class="banner-text">
+				<h2>One stop for <br> all your <span>home solutions</span></h2>
+				<br>
+			<p>Hire the best professionals for<br>any service.</p>
+			<br><br>
+			
+			<a href ="services.php" type="button" class="btn btn-warning btn-block btn-flat">Hire Now</a>
+			</div>
+	</div>
+
+	<br><br>
+
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+
+<div class="inner">    
+	<h1>Popular On Fixups</h1>
+	<div class="border"></div>
+	
+
+		<div class="container-pop">
+			<div class="row">
+				<div class="col-md-4 col-sm-6">
+					<div class="card-pop">
+						<a href="chooseService.php">
+						<img src="images/ac.jpg" alt="">
+						<div class="card-text">
+					<h5>A/C Service</h5>
+		    		</div>
+					</div>
+				</div>
+
+	<div class="col-md-4 col-sm-6">
+					<div class="card-pop">
+						<a href="cleaning.php"></a>
+						<img src="images/cleaning.jpg" alt="">
+						<div class="card-text">
+					<h5>Cleaning Service</h5>
+		    </div>
+		</div>
+	</div>
+
+	<div class="col-md-4 col-sm-6">
+					<div class="card-pop">
+						<a href="E&Wiring.php"></a>
+						<img src="images/electrian.jpg" alt="">
+						<div class="card-text">
+					<h5>Electrical & Wiring</h5>
+		    </div>
+		</div>
+	</div>
+
+</div>
+</div>
+</div>
+</div>
+</div>
+
+
+<br><br>
+
+<div class="inner">    
+	<h1>How to use Fixups?</h1>
+	<div class="border"></div>
+<div class="works">
+	<div class="works-conL"><h1>1</h1><br><img class ="works-img" src = "images/works.jpg"
+		width="100%"/><p>Select a service you need.</p>
+	</div>
+	<div class="works-conM"><h1>2</h1><br><img class ="works-img" src = "images/works(2).jfif"
+		width="100%"/><p>Fill in the request form. The service provider will contact you to reconfirm with you.</p>
+	</div>
+	<div class="works-conR"><h1>3</h1><br><img class ="works-img" src = "images/works(3).jpg"
+		width="100%"/><p>Contactless payment is more advisable.</p>
+	</div>
+
+</div>
+
+
+<div class="testimonial">	  
+
+	<h1>What Our Customer Says</h1>
+		<div class="border"></div>
+	
+	<!--Carousel Slider -->
+	<div class="col-md-12">
+	<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+		<!--Indicators -->
+  <ol class="carousel-indicators">
+    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+  </ol>
+
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+			<img src= "images/face1.png"class="rounded-circle mx-auto d-block" width ="100" alt="">
+			<div class="name"> Abu Bakar </div>
+			<h2> Ordered A/C Service</h2>
+			<div class="stars">
+				<i class="fas fa-star"></i>
+				<i class="fas fa-star"></i>
+				<i class="fas fa-star"></i>
+				<i class="fas fa-star"></i>
+				<i class="fas fa-star"></i>
+			</div>
+  	<p> "Good work. I will definitely refer them again for other services.Thank you Fixups"</p>
+			
+		</div>
+
+<div class="carousel-item">
+			<img src= "images/face3.jpg"class="rounded-circle mx-auto d-block" width ="100" alt="">
+			<div class="name"> Mellisa Tan </div>
+			<h2> Ordered electrical & Wiring service</h2>
+			<div class="stars">
+				<i class="fas fa-star"></i>
+				<i class="fas fa-star"></i>
+				<i class="fas fa-star"></i>
+				<i class="fas fa-star"></i>
+				<i class="far fa-star"></i>
+			</div>
+  	<p>"They are so friendly and provided good quality and reasonable price."</p>	
+</div>
+
+<div class="carousel-item">
+			<img src= "images/face2.png"class="rounded-circle mx-auto d-block" width ="100" alt="">
+			<div class="name"> Kishhore </div>
+			<h2> Ordered plumbing service</h2>
+			<div class="stars">
+				<i class="fas fa-star"></i>
+				<i class="fas fa-star"></i>
+				<i class="fas fa-star"></i>
+				<i class="fas fa-star"></i>
+				<i class="fas fa-star"></i>
+			</div>
+  	<p> "Great job Fixups! I just love their service. Highly recommended."</p>
+			
+</div>
+  </div>
+    
+</div>
+  <a class="carousel-control-prev" href ="#carouselExampleIndicators" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href ="#carouselExampleIndicators" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+</div>
+</div>
+</div>
+
+<!--Jquery JS -->
+<script src="js/jquery-3.4.0js"></script>
+<!-- Bootstrap JS -->
+<script src="js/bootstrap.min.js"></script>
+
+<!-- Footer of website -->
+
+<footer>
+	<div class="footer-content">
+		<h3>About Us</h3>
+		<p> Fixups is the best way to hire local service providers for your home or office maintenance. We provide a wide selection of home services in Penang.</p>
+		
+		<ul class="socials">
+			<li><a href="#"><i class="fa fa-facebook"></i></a></li>
+			<li><a href="#"><i class="fa fa-instagram"></i></a></li>
+			<li><a href="#"><i class="fa fa-twitter"></i></a></li>
+		</ul>
+		
+	</div>
+	<div class="footer-bottom">
+		<p>COPYRIGHT &copy;2021 FIXUPS. ALL RIGHTS RESERVED</p>
+	</div>
+</footer>
+
+
+
+
+
+
