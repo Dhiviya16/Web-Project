fd4958c600d61a805265eaab1263a8357a9a089b
Dhiviya16 <82600789+Dhiviya16@users.noreply.github.com>
2021-07-18 16:31:52 +0800

    Update hire.php

--- hire.php
+++ hire.php
@@ -107,7 +107,7 @@
 			<li><a href="#"><i class="fa fa-facebook"></i></a></li>
 			<li><a href="#"><i class="fa fa-instagram"></i></a></li>
 			<li><a href="#"><i class="fa fa-twitter"></i></a></li>
-		</u1>
+		</ul>
 		
 		</div>
 	<div class="footer-bottom">
